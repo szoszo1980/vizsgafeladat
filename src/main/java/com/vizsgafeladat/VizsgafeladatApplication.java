7bd59d0a3280cbd9726956b20064b9aea07cd39d
szlobodnyik@any.hu <szsz>
2022-10-13 11:52:58 +0200

    Git init

---
+++ src/main/java/com/vizsgafeladat/VizsgafeladatApplication.java
@@ -0,0 +1,13 @@
+package com.vizsgafeladat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizsgafeladatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizsgafeladatApplication.class, args);
+	}
+
+}
